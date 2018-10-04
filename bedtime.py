6e41edf5576105fb1b4173688e801d27f98708eb
MateoElProgramador <mharding2@sheffield.ac.uk>
2018-10-05 00:46:12 +0100

    Setting y ticks put into own method, added weekly view (since last Mon)

--- bedtime.py
+++ bedtime.py
@@ -44,8 +44,8 @@ def sanitiseTime(time):
 #def validateTime(time):
 
 
-def getAverageBedtime(times):
-    timeVals = list( map (getTimeValue, times) )
+def getAverageBedtime(timeVals):
+    #timeVals = list( map (getTimeValue, times) )
     return stat.mean(timeVals)
 
 
@@ -96,35 +96,60 @@ def main():
 
 
 # Plots graph of bedtimes over time:
-# TODO Make y axis labels round times (e.g. 1:00, 2:00 etc.)
-
 def plotBedtimes(times):
     # Gets timeVals of time strings from txt file:
     timeVals = list( map (getTimeValue, times) )
     print(f"TimeVals: {timeVals}")
 
+    view = "weekly"
+
     xTicks = []
     xLabels = []
-
-
+    dates = []
     FIRST_DATE = datetime.date(2018, 9, 15)     # First date that bedtime is recorded
+
+    dates.append(FIRST_DATE)
     xLabels.append(FIRST_DATE.strftime('%d/%m'))    # The first xLabel is assigned
     xTicks.append(1)    # xTicks seems to need a list of numbers of length len(timeVals)
 
     date = FIRST_DATE
 
+    # date is incremented by a day and appended to xLabels and dates:
     for i in range(2, len(timeVals)+1):
         date += datetime.timedelta(days=1)  # Date is incremented by 1 day
+        dates.append(date)
         xLabels.append(date.strftime('%d/%m'))
         xTicks.append(i)
 
+
+    # For weekly view (since last Monday):
+    if (view == "weekly"):
+        # Get all days for dates:
+        days = [d.strftime('%A') for d in dates]
+        # Find position in dates where the last Monday features:
+        lastMonPos = (len(days) - 1) - (list(reversed(days)).index("Monday"))
+        print(days)
+        print("Position of last Monday: " + str(lastMonPos))
+
+        # Slice lists to only account for since last Monday:
+        xTicks = xTicks[lastMonPos:]
+        xLabels = xLabels[lastMonPos:]
+        timeVals = timeVals[lastMonPos:]
+
+
     plt.xticks(xTicks, xLabels)
 
-    plt.title(f"Average time: {timeValToTime(getAverageBedtime(times))}")
+    plt.title(f"Average time: {timeValToTime(getAverageBedtime(timeVals))}")
     plt.plot(xTicks, timeVals)
 
-    print("Current y lim: " + str(plt.ylim()))
+    #print("Current y lim: " + str(plt.ylim()))
 
+    setYTicks(timeVals)
+    plt.show()
+
+
+# Finds and sets the labels for the y axis, using round times:
+def setYTicks(timeVals):
     yTicks = []
     minTimeLim, maxTimeLim = plt.ylim()
 
@@ -146,16 +171,12 @@ def plotBedtimes(times):
         time += Y_AXIS_INCREMENT
         yTicks.append(time)
 
-    #ylocs, _ = plt.yticks() # Gets values used as y axis
-
     # Creates a list of time strings converted from y axis values (list comprehension!):
     yLabels = [timeValToTime(y) for y in yTicks]
 
     # Despite documentation referring to an array of Text objects for labels,
     # a list of strings will serve:
     plt.yticks(yTicks, yLabels)  # Sets time labels for y axis!
-    plt.show()
-
 
 
 def filterList(list):
